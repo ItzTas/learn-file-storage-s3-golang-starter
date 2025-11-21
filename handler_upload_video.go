189fcd744c91d64bd250f79872b0c6c2065bc0b5
Tales <59647566+ItzTas@users.noreply.github.com>
2025-11-21 19:44:34 -0400

    chore: user auth

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
 
@@ -12,6 +13,17 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not parse video id", err)
+		return
+	}
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not get bearer token", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Could not validade user", err)
 		return
 	}
 }
